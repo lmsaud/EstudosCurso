0c95912321867e1e7c58dab44cda816920124202
portuga <lucas.m.saud@icloud.com>
2018-03-05 23:02:40 -0300

    Primeiro script em C para realizar a conexao com o BD MySQL

--- main.c
+++ main.c
@@ -20,7 +20,33 @@ void connectDB(void) {
     }
 };
 
-void createDB (void) {
+void createTables (void);
+
+void createTables(void) {
+    if(mysql_query(&connection,
+                   "CREATE TABLE IF NOT EXISTS users (\n"
+                           "    id int(11) NOT NULL,\n"
+                           "    username varchar(255) NOT NULL,\n"
+                           "    first_name varchar(255) NOT NULL,\n"
+                           "    last_name varchar(255) NOT NULL,\n"
+                           "    email varchar(255) NOT NULL,\n"
+                           "    password varchar(32) NOT NULL,\n"
+                           "    sign_up_date date NOT NULL,\n"
+                           "    activated enum('0','1') NOT NULL,\n"
+                           "    PRIMARY KEY (id)\n"
+                           ") ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;") == 0) {
+        printf("Table created. \n");
+    }
+    else {
+        printf("Table creation failed. ");
+        printf("MySQL error message: %s\n", mysql_error(&connection));
+        exit(1);
+    }
+}
+
+void createDB (void);
+
+void createDB(void) {
     if(mysql_query(&connection, "USE users_database") == 0) {
         printf("Banco de dados já existe.\n");
     } else {
@@ -32,27 +58,10 @@ void createDB (void) {
             printf("MySQL error message: %s\n", mysql_error(&connection));
             exit(1);
         }
+        createTables();
     }
 
-    if (mysql_query(&connection,
-                    "CREATE TABLE IF NOT EXISTS users (\n"
-                            "    id int(11) NOT NULL,\n"
-                            "    username varchar(255) NOT NULL,\n"
-                            "    first_name varchar(255) NOT NULL,\n"
-                            "    last_name varchar(255) NOT NULL,\n"
-                            "    email varchar(255) NOT NULL,\n"
-                            "    password varchar(32) NOT NULL,\n"
-                            "    sign_up_date date NOT NULL,\n"
-                            "    activated enum('0','1') NOT NULL,\n"
-                            "    PRIMARY KEY (id)\n"
-                            ") ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;") == 0) {
-        printf("Table created. \n");
-    }
-    else {
-        printf("Table creation failed. ");
-        printf("MySQL error message: %s\n", mysql_error(&connection));
-        exit(1);
-    }
+
 
     mysql_close(&connection);
 }
