2152acc2b6f941211698f6e57b034de40a5dd1af
portuga <lucas.m.saud@icloud.com>
2018-03-06 18:48:27 -0300

    Primeiro script em C para realizar a conexao com o BD MySQL

--- main.c
+++ main.c
@@ -4,6 +4,13 @@
 
 MYSQL connection;
 
+void finishWithError()
+{
+    fprintf(stderr, "%s\n", mysql_error(&connection));
+    mysql_close(&connection);
+    exit(1);
+}
+
 void connectDB(void) {
     if (mysql_init(&connection) == NULL) {
         printf("Erro ao iniciar a conexão com o servidor MySQL!\n");
@@ -23,8 +30,11 @@ void connectDB(void) {
 void createTables (void);
 
 void createTables(void) {
+    if (mysql_query(&connection, "DROP TABLE IF EXISTS Detalhes")) {
+        finishWithError();
+    }
     if(mysql_query(&connection,
-                   "CREATE TABLE IF NOT EXISTS users (\n"
+                   "CREATE TABLE IF NOT EXISTS Detalhes (\n"
                            "    id int(11) NOT NULL,\n"
                            "    username varchar(255) NOT NULL,\n"
                            "    first_name varchar(255) NOT NULL,\n"
@@ -47,26 +57,28 @@ void createTables(void) {
 void createDB (void);
 
 void createDB(void) {
-    if(mysql_query(&connection, "USE users_database") == 0) {
+    if(mysql_query(&connection, "USE Clientes") == 0) {
         printf("Banco de dados já existe.\n");
     } else {
-        if (mysql_query(&connection, "CREATE DATABASE users_database") == 0) {
+        if (mysql_query(&connection, "CREATE DATABASE Clientes") == 0) {
+
             printf("Banco de dados criado com sucesso.\n");
+            createTables();
         }
         else {
             printf("Falha na criação do banco de dados. ");
             printf("MySQL error message: %s\n", mysql_error(&connection));
             exit(1);
         }
-        createTables();
+
     }
-
-
-
     mysql_close(&connection);
 }
 
 int main(int argc, char **argv) {
+    int sysSize = sizeof(int);
+    printf("int size is: %d bytes\n", sysSize);
+    printf("MySQL client version: %s\n", mysql_get_client_info());
 
     connectDB();
 
